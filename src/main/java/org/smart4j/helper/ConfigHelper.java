2e32514020f17497e0eb287c8f755bb949833235
cuicui <515702512@qq.com>
2018-08-31 23:32:45 +0800

    smart-framework

--- src/main/java/org/smart4j/helper/ConfigHelper.java
+++ src/main/java/org/smart4j/helper/ConfigHelper.java
@@ -2,9 +2,18 @@ package org.smart4j.helper;
 
 import java.util.Properties;
 import org.smart4j.framework.ConfigConstant;
+import org.smart4j.framework.util.PropsUtil;
 
 /**
  * 属性文件助手
  */
-public class ConfigHelper {
+public final class ConfigHelper {
+
+        private static final Properties CONFIG_PROPS =
+                PropsUtil.loadProps(ConfigConstant.CONFIG_FILE);
+
+    /**
+     * 获取JDBC驱动
+     */
+    
 }
