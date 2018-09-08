fd64ca4bda15d2756782035397205335745d8891
swordcui <38028121+swordcui@users.noreply.github.com>
2018-09-08 10:13:26 +0800

    smart-framework

---
+++ src/main/java/org/smart4j/framework/util/ReflectionUtil.java
@@ -0,0 +1,22 @@
+package org.smart4j.framework.util;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * 反射工具类
+ */
+public final class ReflectionUtil {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReflectionUtil.class);
+
+    /**
+     * 创建实例
+     */
+    public static Object newInstance(Class<?> cls) {
+        Object instance;
+        
+    }
+}
