a809ca8b3c7081c591ea2d1c61cc8ec1d09398b5
cuicui <515702512@qq.com>
2018-09-10 22:43:10 +0800

    smart-framework

--- src/main/java/org/smart4j/framework/helper/IocHelper.java
+++ src/main/java/org/smart4j/framework/helper/IocHelper.java
@@ -15,7 +15,29 @@ public final class IocHelper {
         //获取所有的Bean类与Bean实例之间的映射关系（简称Bean Map)
         Map<Class<?>,Object> beanMap = BeanHelper.getBeanMap();
         if(CollectionUtil.isNotEmpty(beanMap)) {
-            for (Map.Entry<Class<?>,Object>beanEntry:beanMap.entrySet());
+            for (Map.Entry<Class<?>,Object> beanEntry : beanMap.entrySet() ){
+                //从BeanMap中获取Bean类与Bean实例
+                Class<?> beanClass = beanEntry.getKey();
+                Object beanInstance = beanEntry.getValue();
+                //获取Bean类中定义的所有成员变量（简称Bean Field)
+                Field[] beanFields = beanClass.getDeclaredFields();
+                if(ArrayUtil.isNotEmpty(beanFields)) {
+                    //遍历beanField
+                    for(Field beanField : beanFields) {
+                        //判断当前Bean Field是否带有Inject注解
+                        if(beanField.isAnnotationPresent(Inject.class)) {
+                            //在BeanMap中获取Bean Field对应的实例
+                            Class<?> beanFieldClass = beanField.getType();
+                            Object beanFieldInstance = beanMap.get(beanFieldClass);
+                            if (beanFieldInstance != null ) {
+                                //通过反射初始化BeanField的值
+                                ReflectionUtil.setField(beanInstance,beanField,beanFieldInstance);
+                            }
+                        }
+                    }
+                }
+            }
+
         }
     }
 }
