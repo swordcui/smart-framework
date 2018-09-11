81dd41df1ca99863a6b84f56441ab0d1ba9e3b69
cuicui <515702512@qq.com>
2018-09-11 23:24:24 +0800

    smart-framework

--- src/main/java/org/smart4j/framework/DispatcherServlet.java
+++ src/main/java/org/smart4j/framework/DispatcherServlet.java
@@ -6,10 +6,8 @@ import java.lang.reflect.Method;
 import java.util.Enumeration;
 import java.util.Map;
 import java.util.HashMap;
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
+import javax.jws.WebService;
+import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -23,7 +21,37 @@ import org.smart4j.framework.helper.ConfigHelper;
 import org.smart4j.framework.helper.ControllerHelper;
 import org.smart4j.framework.util.ArrayUtil;
 import org.smart4j.framework.util.ReflectionUtil;
+import org.smart4j.framework.util.JsonUtil;
+import org.smart4j.framework.util.StreamUtil;
+import org.smart4j.framework.util.CodecUtil;
+import org.smart4j.framework.util.StringUtil;
 
 
-public class DispatcherServlet {
+/**
+ * 请求转发器
+ */
+
+@WebServlet(urlPatterns = "/*",loadOnStartup = 0)
+public class DispatcherServlet extends HttpServlet{
+
+    @Override
+    public void init() throws ServletException {
+        //初始化相关Helper类
+        HelperLoader.init();
+        //获取ServletContext对象（用于注册Servlet)
+        ServletContext servletContext = getServletConfig().getServletContext();
+        //注册处理JSP的Servlet
+        ServletRegistration jspServlet = servletContext.getServletRegistration("jsp");
+        jspServlet.addMapping(ConfigHelper.getAppJspPath() + "*");
+        //注册处理静态资源的默认Servlet
+        ServletRegistration defaultServlet = servletContext.getServletRegistration("default");
+        defaultServlet.addMapping(ConfigHelper.getAppAssetPath() + "*")
+    }
+
+    @Override
+    public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //获取请求方法与路径
+        String requestMethod = request.getMethod().toLowerCase();
+        String requestPath = request.getPathInfo();
+    }
 }
