fcd08bc698c6c43f8208c939b806968af9b7121e
swordcui <38028121+swordcui@users.noreply.github.com>
2018-09-12 15:07:30 +0800

    smart-framework

--- src/main/java/org/smart4j/framework/DispatcherServlet.java
+++ src/main/java/org/smart4j/framework/DispatcherServlet.java
@@ -12,10 +12,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.smart4j.framework.bean.Data;
-import org.smart4j.framework.bean.Handler;
-import org.smart4j.framework.bean.Param;
-import org.smart4j.framework.bean.View;
+
+import org.smart4j.framework.bean.*;
 import org.smart4j.framework.helper.BeanHelper;
 import org.smart4j.framework.helper.ConfigHelper;
 import org.smart4j.framework.helper.ControllerHelper;
@@ -45,7 +43,7 @@ public class DispatcherServlet extends HttpServlet{
         jspServlet.addMapping(ConfigHelper.getAppJspPath() + "*");
         //注册处理静态资源的默认Servlet
         ServletRegistration defaultServlet = servletContext.getServletRegistration("default");
-        defaultServlet.addMapping(ConfigHelper.getAppAssetPath() + "*")
+        defaultServlet.addMapping(ConfigHelper.getAppAssetPath() + "*");
     }
 
     @Override
@@ -53,5 +51,70 @@ public class DispatcherServlet extends HttpServlet{
         //获取请求方法与路径
         String requestMethod = request.getMethod().toLowerCase();
         String requestPath = request.getPathInfo();
+        //获取Action处理器
+        Handler handler = ControllerHelper.getHandler(requestMethod,requestPath);
+        if (handler != null ) {
+            //获取Controller类及其Bean实例
+            Class<?> controllerClass = handler.getControllerClass();
+            Object controllerBean = BeanHelper.getBean(controllerClass);
+            //创建请求参数对象
+            Map<String,Object> paramMap = new HashMap<String, Object>();
+            Enumeration<String> paramNames = request.getParameterNames();
+            while (paramNames.hasMoreElements()) {
+                String paramName =paramNames.nextElement();
+                String paramValue = request.getParameter(paramName);
+                paramMap.put(paramName,paramValue);
+            }
+            String body = CodecUtil.decodeURL(StreamUtil.getString(request.getInputStream()));
+            if (StringUtil.isNotEmpty(body)) {
+                String[] params = StringUtil.splitString(body,"&");
+                if (ArrayUtil.isNotEmpty(params)) {
+                    for (String param : params) {
+                        String[] array =StringUtil.splitString(param,"=");
+                        if (ArrayUtil.isNotEmpty(array) && array.length==2) {
+                            String paramName =array[0];
+                            String paramValue= array[1];
+                            paramMap.put(paramName,paramValue);
+                        }
+                    }
+                }
+            }
+
+
+            Param param = new Param(paramMap);
+            //调用Acation方法
+            Method actionMethod = handler.getActionMethod();
+            Object result = ReflectionUtil.invokeMethod(controllerBean,actionMethod,param);
+            //处理Action返回值
+            if (request instanceof View) {
+                //返回JSP页面
+                View view = (View) request;
+                String path = view.getPath();
+                if (StringUtil.isNotEmpty(path)) {
+                    if (path.startsWith("/")){
+                        response.sendRedirect(request.getContextPath() + path);
+                    }else {
+                        Map<String,Object> model = view.getModel();
+                        for(Map.Entry<String, Object>entry : model.entrySet()) {
+                            request.setAttribute(entry.getKey(),entry.getValue());
+                        }
+                        request.getRequestDispatcher(ConfigHelper.getAppJspPath() +path).forward(request,response);
+                    }
+                }
+            }else if (result instanceof Data) {
+                //返回JSON数据
+                Data data = (Data) result;
+                Object model = data.getModel();
+                if (model != null ) {
+                    response.setContentType("application/json");
+                    response.setCharacterEncoding("UTF-8");
+                    PrintWriter writer = response.getWriter();
+                    String json = JsonUtil.toJson(model);
+                    writer.write(json);
+                    writer.flush();
+                    writer.close();
+                }
+            }
+        }
     }
 }
--- src/main/java/org/smart4j/framework/util/StringUtil.java
+++ src/main/java/org/smart4j/framework/util/StringUtil.java
@@ -11,8 +11,29 @@ public final class StringUtil {
      * 判断字符串是否为空
      */
     public static boolean isEmpty(String str) {
-        if (str != null ) {
+        if (str != null) {
             str = str.trim();
         }
         return StringUtils.isEmpty(str);
     }
+
+    /**
+     * 判断字符串是否非空
+     */
+    public static boolean isNotEmpty(String str) {
+        if (str != null) {
+            str= str.trim();
+        }
+        return StringUtils.isNotEmpty(str);
+    }
+
+
+    /**
+     * 分割字符串
+     */
+
+    public static String[] splitString(String str  ,String separatorChars) {
+        return StringUtils.split(str,separatorChars);
+    }
+
+}
